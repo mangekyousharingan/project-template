3df20752dfdc181c0c4f5586a3233b6449e9b35d
Sebastian Wdowiarz <wdowiarzsebastian@gmail.com>
2025-04-13 15:18:13 +0200

    lint check

--- src/adapters/database/session.py
+++ src/adapters/database/session.py
@@ -1,16 +1,12 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
 from typing import Generator
 
-from src.adapters.database.models import Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
 from src.core.config import settings
 
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
 
 
 def get_db() -> Generator[Session, None, None]:
@@ -18,4 +14,4 @@ def get_db() -> Generator[Session, None, None]:
     try:
         yield db
     finally:
-        db.close() 
+        db.close()
